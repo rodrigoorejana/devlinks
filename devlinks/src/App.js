467e94a1d9784379169378865a09c0bde7badf1d
rodrigo <rodrigoorejana.rc@gmail.com>
2026-03-11 21:10:23 -0300

    Initial Next.js project setup

---
+++ devlinks/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import SinglePage from './SinglePage';
+function App() {
+  return (
+    <div className="App">
+      <SinglePage />
+    </div>
+  );
+}
+
+export default App;
---
+++ devlinks/src/SinglePage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+const SinglePage = () => {
+    return (
+        <div>
+            <h1>Single Page</h1>
+        </div>
+    );
+}
+export default SinglePage;
